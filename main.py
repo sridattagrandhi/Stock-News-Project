f530b94b5a50c2201d5aeadb109789e1f9ded819
Sridatta Grandhi <sridatta963@gmail.com>
2023-08-24 14:48:09 -0700

    deleted email

--- main.py
+++ main.py
@@ -88,17 +88,6 @@ if diff_percent > 1:
             from_='+18447650736',
             to='+19708449749'
         )
-    
-    for article in formatted_articles:
-        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-            connection.ehlo()
-            connection.starttls()
-            connection.login(user=my_email, password=pswrd)
-            connection.sendmail(
-                from_addr=my_email,
-                to_addrs="raji321@gmail.com",
-                msg=article
-            )
             
 
 #Optional TODO: Format the message like this: 
